f4dd74c9ac4d81d2c93bfb84e23ecf38050f7d13
MrTrojan52 <kirill_131097@mail.ru>
2018-12-01 04:40:01 +0300

    Some adds

--- app/src/main/java/com/example/trojan52/mqttsmartdevicecontroller/MainActivity.java
+++ app/src/main/java/com/example/trojan52/mqttsmartdevicecontroller/MainActivity.java
@@ -8,13 +8,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class MainActivity extends AppCompatActivity {
-    ConnectionData cData;
-
+    private ConnectionData cData;
+    private Button btnSub;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,9 +48,72 @@ public class MainActivity extends AppCompatActivity {
                 getApplicationContext().getString(R.string.connect_user_key),
                 getApplicationContext().getString(R.string.connect_pass_key)
         );
+        btnSub = (Button)findViewById(R.id.connect);
+
+
+
+
+
         if(cData.hasEmptyValues()) {
             Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
             startActivity(intent);
+        } else {
+//            String clientId = MqttClient.generateClientId();
+//            final MqttAndroidClient client =
+//                    new MqttAndroidClient(this.getApplicationContext(), "tcp://"+ cData.getHost() + ":" + cData.getPort(),
+//                            clientId);
+//
+//
+//
+//            try {
+//                MqttConnectOptions options = new MqttConnectOptions();
+//                options.setUserName(cData.getUserName());
+//                options.setPassword(cData.getPassword().toCharArray());
+//                IMqttToken token = client.connect(options);
+//                token.setActionCallback(new IMqttActionListener() {
+//                    @Override
+//                    public void onSuccess(IMqttToken asyncActionToken) {
+//                        // We are connected
+//                        System.out.println("CONNECTED");
+//                    }
+//
+//                    @Override
+//                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+//                        // Something went wrong e.g. connection timeout or firewall problems
+//                        //Log.d(TAG, "onFailure");
+//                        System.out.println("NOT - CONNECTED");
+//
+//                    }
+//                });
+//            } catch (MqttException e) {
+//                e.printStackTrace();
+//            }
+
+//            btnSub.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    String topic = cData.getUserName() + "feeds/kitchen.lamp-on-the-kitchen-1";
+//                    int qos = 1;
+//                    try {
+//                        final IMqttToken subToken = client.subscribe(topic, qos);
+//                        subToken.setActionCallback(new IMqttActionListener() {
+//                            @Override
+//                            public void onSuccess(IMqttToken asyncActionToken) {
+//                                System.out.println("SUBSCRIBED");
+//                            }
+//
+//                            @Override
+//                            public void onFailure(IMqttToken asyncActionToken,
+//                                                  Throwable exception) {
+//                                System.out.println("NOT - SUBSCRIBED");
+//
+//                            }
+//                        });
+//                    } catch (MqttException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            });
         }
 
     }
